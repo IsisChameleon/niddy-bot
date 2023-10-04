6b0871914f4c2956e18dfc83f00bafae2d465803
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2023-10-04 09:46:31 +0000

    tools.ipynb experiments with nebulagraph

--- src/helpers.py
+++ src/helpers.py
@@ -1,23 +1,23 @@
 import pickle
 
-def pickleSave(object, name, folder='.', silent=False):
 
-    filename = folder + '/' + name + '.pkl'
-    if silent==False:
-        print('Saving object {} to pickle file {}'.format(name, filename))
-    with open(filename, mode='wb') as fipkl:    
+def pickleSave(object, name, folder=".", silent=False):
+    filename = folder + "/" + name + ".pkl"
+    if silent == False:
+        print("Saving object {} to pickle file {}".format(name, filename))
+    with open(filename, mode="wb") as fipkl:
         pickle.dump(object, fipkl)
-        
+
+
 def pickleLoad(name, folder: str, silent=False):
-    
-    filename = folder + '/' + name + '.pkl'
-    if silent==False:
-        print('Loading object {} from pickle file {}'.format(name, filename))
-    
+    filename = folder + "/" + name + ".pkl"
+    if silent == False:
+        print("Loading object {} from pickle file {}".format(name, filename))
+
     try:
-        with open(filename, mode='rb') as fipkl:
-            myObject=pickle.load(fipkl)
+        with open(filename, mode="rb") as fipkl:
+            myObject = pickle.load(fipkl)
         return myObject
     except IOError:
-        print('Pickle file {} not found, returning None object'.format(filename))
+        print("Pickle file {} not found, returning None object".format(filename))
         return None
--- src/modules/helpers.py
+++ src/modules/helpers.py
@@ -2,18 +2,41 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 
+import pickle
+
+
 def load_api_key():
     """
-    Loads the OpenAI API key 
+    Loads the OpenAI API key
     """
-    user_api_key = st.sidebar.text_input(label="#### Your OpenAI API key 👇", placeholder="sk-...", type="password")
+    user_api_key = st.sidebar.text_input(
+        label="#### Your OpenAI API key 👇", placeholder="sk-...", type="password"
+    )
     if user_api_key:
         st.sidebar.success("API key loaded from sidebar", icon="🚀")
         return user_api_key
-    
+
     load_dotenv(override=True)
     return os.getenv("OPENAI_API_KEY")
 
-    
 
-    
+def pickleSave(object, name, folder=".", silent=False):
+    filename = folder + "/" + name + ".pkl"
+    if silent == False:
+        print("Saving object {} to pickle file {}".format(name, filename))
+    with open(filename, mode="wb") as fipkl:
+        pickle.dump(object, fipkl)
+
+
+def pickleLoad(name, folder: str, silent=False):
+    filename = folder + "/" + name + ".pkl"
+    if silent == False:
+        print("Loading object {} from pickle file {}".format(name, filename))
+
+    try:
+        with open(filename, mode="rb") as fipkl:
+            myObject = pickle.load(fipkl)
+        return myObject
+    except IOError:
+        print("Pickle file {} not found, returning None object".format(filename))
+        return None
